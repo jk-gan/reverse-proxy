2958c5b94c5b004763b30c6fa7a3ee7a620aac5d
jk-gan <kuhn96@gmail.com>
2021-01-01 13:11:41 +0800

    feat: add status page to check how many request we proxied

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::*;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Client, Request, Server};
+use hyper::{Body, Client, Request, Response, Server};
 use std::net::SocketAddr;
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 
 fn mutate_request(req: &mut Request<Body>) -> Result<()> {
     for key in &[
@@ -27,25 +27,40 @@ fn mutate_request(req: &mut Request<Body>) -> Result<()> {
     Ok(())
 }
 
+#[derive(Debug)]
+struct Stats {
+    proxied: usize,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let https = hyper_rustls::HttpsConnector::with_native_roots();
     let client: Client<_, hyper::Body> = Client::builder().build(https);
     let client = Arc::new(client);
+    let stats: Arc<RwLock<Stats>> = Arc::new(RwLock::new(Stats { proxied: 0 }));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
     let make_svc = make_service_fn(move |_| {
         let client = Arc::clone(&client);
+        let stats = Arc::clone(&stats);
         async move {
             Ok::<_, Error>(service_fn(move |mut req| {
                 let client = Arc::clone(&client);
+                let stats = Arc::clone(&stats);
                 async move {
-                    mutate_request(&mut req)?;
-                    client
-                        .request(req)
-                        .await
-                        .context("Making request to backend server")
+                    if req.uri().path() == "/status" {
+                        let stats: &Stats = &*stats.read().unwrap();
+                        let body: Body = format!("{:?}", stats).into();
+                        Ok(Response::new(body))
+                    } else {
+                        stats.write().unwrap().proxied += 1;
+                        mutate_request(&mut req)?;
+                        client
+                            .request(req)
+                            .await
+                            .context("Making request to backend server")
+                    }
                 }
             }))
         }
