0e2880d2434b36691aa384e316d32cf12e0274db
jk-gan <kuhn96@gmail.com>
2021-01-01 12:53:38 +0800

    feat: mutate the req to redirect to snoyman website

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
 use anyhow::*;
-use hyper::Client;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Client, Request, Server};
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+fn mutate_request(req: &mut Request<Body>) -> Result<()> {
+    for key in &[
+        "content-length",
+        "transfer-encoding",
+        "accept-encoding",
+        "content-encoding",
+    ] {
+        req.headers_mut().remove(*key);
+    }
+
+    let uri = req.uri();
+    let uri_string = match uri.query() {
+        None => format!("https://www.snoyman.com{}", uri.path()),
+        Some(query) => format!("https://www.snoyman.com{}?{}", uri.path(), query),
+    };
+
+    *req.uri_mut() = uri_string
+        .parse()
+        .context("Parsing URI in mutate_request")?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let https = hyper_rustls::HttpsConnector::with_native_roots();
     let client: Client<_, hyper::Body> = Client::builder().build(https);
+    let client = Arc::new(client);
 
-    let url = "https://httpbin.org/json".parse().context("Parsing URL")?;
-    let res = client.get(url).await.context("Performing HTTP request")?;
-    println!("{:?}", res);
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
-    let body = res.body();
-    println!("body: {:?}", body);
+    let make_svc = make_service_fn(move |_| {
+        let client = Arc::clone(&client);
+        async move {
+            Ok::<_, Error>(service_fn(move |mut req| {
+                let client = Arc::clone(&client);
+                async move {
+                    mutate_request(&mut req)?;
+                    client
+                        .request(req)
+                        .await
+                        .context("Making request to backend server")
+                }
+            }))
+        }
+    });
+    Server::bind(&addr)
+        .serve(make_svc)
+        .await
+        .context("Running server")?;
 
-    Ok(())
+    Ok::<(), anyhow::Error>(())
 }
