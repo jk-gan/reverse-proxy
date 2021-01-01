ea279b3aeea5a8d5b4f60f44fb95d6017f262130
jk-gan <kuhn96@gmail.com>
2021-01-01 11:22:14 +0800

    feat: add anyhow for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use anyhow::*;
+use hyper::Client;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    let https = hyper_rustls::HttpsConnector::with_native_roots();
+    let client: Client<_, hyper::Body> = Client::builder().build(https);
+
+    let url = "https://httpbin.org/json".parse().context("Parsing URL")?;
+    let res = client.get(url).await.context("Performing HTTP request")?;
+    println!("{:?}", res);
+
+    let body = res.body();
+    println!("body: {:?}", body);
+
+    Ok(())
 }
